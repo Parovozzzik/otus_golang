cebc9e1ce782b9409263b82f9f3e48a4c2de266e
v.arsentev <v.arsentev@crpt.ru>
2022-05-21 01:50:35 +0300

    HW03.

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordsStruct struct {
+	word  string
+	count int
+}
+
+func Top10(text string) []string {
+	text = prepareText(text)
+
+	wordsMap := strings.Fields(text)
+	wordsCountMap := map[string]int{}
+	for _, word := range wordsMap {
+		wordsCountMap[word]++
+	}
+
+	wordsSlice := make([]wordsStruct, 0, len(wordsCountMap))
+	for k, v := range wordsCountMap {
+		wordsSlice = append(wordsSlice, wordsStruct{k, v})
+	}
+
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		if wordsSlice[i].count == wordsSlice[j].count {
+			return wordsSlice[i].word < wordsSlice[j].word
+		}
+		return wordsSlice[i].count > wordsSlice[j].count
+	})
+
+	return prepareResult(wordsSlice)
+}
+
+func prepareText(text string) string {
+	text = strings.ToLower(text)
+	replacer := strings.NewReplacer(
+		" - ", " ",
+		",", "",
+		".", "",
+	)
+	text = replacer.Replace(text)
+
+	return text
+}
+
+func prepareResult(wordsSlice []wordsStruct) []string {
+	var result []string
+	if len(wordsSlice) > 10 {
+		for _, v := range wordsSlice[0:10] {
+			result = append(result, v.word)
+		}
+	}
+
+	return result
 }

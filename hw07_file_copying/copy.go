62e52c3492770a3f78378cbfe42de5371a6e3567
v.arsentev <v.arsentev@crpt.ru>
2022-07-10 13:25:53 +0300

    HW07.

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,64 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	flag.Parse()
+
+	var file *os.File
+	file, err := os.OpenFile(fromPath, os.O_RDWR, 0o644)
+	if err != nil && os.IsNotExist(err) {
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+
+	fi, _ := file.Stat()
+	fileSize := fi.Size()
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	start := int64(0)
+	size := fileSize
+	if offset != 0 {
+		start = offset
+		size = fileSize - offset
+	}
+
+	barCount := 1
+	extraBarCount := 0
+	if limit != 0 {
+		barCount = int(size) / int(limit)
+		if size%limit > 0 {
+			extraBarCount = 1
+		}
+	} else {
+		limit = fileSize
+	}
+
+	bar := pb.StartNew(barCount + extraBarCount)
+	defer bar.Finish()
+
+	data := make([]byte, size)
+	for start < fileSize {
+		bar.Increment()
+		dataCurrent := make([]byte, limit)
+		read, err := file.ReadAt(dataCurrent, start)
+		start += int64(read)
+
+		if err == io.EOF || (start-limit) >= size {
+			copy(data[size-(size%limit):], dataCurrent[:(size%limit)])
+			break
+		} else {
+			copy(data[(start-limit-offset):(start-offset)], dataCurrent[:limit])
+		}
+	}
+
+	emptyFile, _ := os.Create(toPath)
+	defer emptyFile.Close()
+	emptyFile.Write(data)
+
 	return nil
 }

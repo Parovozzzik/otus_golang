67b9e355ce097be0b9dd0b9bbc13610d2157ca0d
v.arsentev <v.arsentev@crpt.ru>
2022-07-25 02:38:07 +0300

    HW08. nosec G204

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -7,6 +7,7 @@ import (
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
+    // #nosec G204
 	command := exec.Command(cmd[0], cmd[1:]...)
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
